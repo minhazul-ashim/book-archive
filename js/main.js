d31487ab9be7ead475bb344bb99bbb0aa9af4dfa
Md.Minhazul Haque <ashim.ashim7470@gmail.com>
2021-09-02 02:17:19 +0600

    cards aligned

--- js/main.js
+++ js/main.js
@@ -30,30 +30,61 @@ const firstPublishExist = (book) => {
     } else {
         return 'Unknown'
     }
+};
+
+const publisherExistence = (book) => {
+
+    if (book.publisher !== undefined) {
+        return book.publisher;
+    } else {
+        return 'Unknown'
+    }
+}
+
+const resultCount = (dis, total, shown) => {
+
+    document.getElementById('total-result').innerText = total;
+    document.getElementById('shown-result').innerText = shown;
+    document.getElementById('result-count').style.display = dis;
+}
+
+const errorMessage = (dis) => {
+
+    document.getElementById('no-result').style.display = dis;
 }
 
 const displayBooks = (data) => {
 
     const bookList = data.docs;
-    console.log(bookList)
+    console.log(data)
     const booksContainer = document.getElementById('books-container');
     booksContainer.textContent = '';
+    errorMessage('none')
+    resultCount('none')
 
-    bookList.forEach(book => {
+    if (data.numFound === 0) {
 
-        const newDiv = document.createElement('div');
-        newDiv.innerHTML = `
-        <div class="col">
-            <div class="card">
-                <img class='img-fluid' src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="">
-                    <div class="card-body">
-                     <h3 class="card-title">${book.title}</h3>
-                     <h6 class="card-text">Author: ${authorExistence(book)}</h6>
-                     <h6 class="card-text">First Published: ${firstPublishExist(book)}</h6>
-                    </div>
+        errorMessage('block')
+
+    } else {
+        bookList.forEach(book => {
+
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = `
+            <div class="col h-100">
+                <div class="card h-100">
+                    <img class='img-fluid' src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                         <h3 class="card-title">${book.title}</h3>
+                         <h6 class="card-text">Author: ${authorExistence(book)}</h6>
+                         <h6 class="card-text">Publisher: ${publisherExistence(book)}</h6>
+                         <h6 class="card-text">First Published: ${firstPublishExist(book)}</h6>
+                        </div>
+                </div>
             </div>
-        </div>
-        `
-        booksContainer.appendChild(newDiv);
-    })
+            `
+            booksContainer.appendChild(newDiv);
+            resultCount('block', data.numFound, bookList.length)
+        });
+    }
 }
